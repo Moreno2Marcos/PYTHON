1f000aa07396fc749d18a5d6e582dbf01cddd242
Iván Moreno <102688868+Moreno2Marcos@users.noreply.github.com>
2022-07-14 17:28:20 -0300

    Add files via upload

---
+++ Python_skills/OPERACOES_BASICAS_01.py
@@ -0,0 +1,8 @@
+# define preço
+precoProduto = 350.26
+# calcula imposto
+imposto = precoProduto * .25  # calcula o imposto
+# calcula preço final
+precoFinal = precoProduto + imposto
+# imprime resposta
+precoFinal
